21437906c75de7a92324176f4a1d5094f9b3bc17
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-01-05 23:44:08 -0300

    CI - channel info

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, HashtagIcon, Title, Separador, Description } from './styles';
+
+
+const ChannelInfo: React.FC = () =>{
+    return (
+        <Container>
+            <HashtagIcon/>
+            <Title>Chat-livre</Title>
+            <Separador />
+            <Description>Canal aberto para conversas</Description>
+        </Container>
+    )
+};
+
+export default ChannelInfo;
